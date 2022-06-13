45c8576c61f7162b1536cb209abebfe9a816e7df
Fabiano Filho <81716443+fabiano-filho@users.noreply.github.com>
2022-06-13 15:48:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once 'class/Livro.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
@@ -6,51 +10,14 @@
         <title>Programação Orientada a Objetos II - PHP</title>
     </head>
     <body>
-        <h1>Hello, World!</h1>
+        <table>
+            <tr>Coluna</tr>
+        <?php
+            $query = Livro::getLivros();
+            while($registro = $query -> fetch()){
+                
+            }
+        ?>        
+        </table>    
     </body>
 </html>
-
-<?php
-require_once './class/Pessoa.php';
-
-try {
-    $query = Pessoa::getPessoa();
-    while($registro = $query -> fetch()) {
-        echo $registro['nome'] . '<br>';
-    }
-    
-} catch(PDOException $e) {
-    echo 'Erro ao conectar: ' . $e -> getMessage();
-}
-
-//include_once "./class/DataBase.php";
-//
-//try {
-//    $db = new DataBase();
-//    $query = $db -> BancoLink -> query("SELECT * from pessoa");
-//    while($registro = $query -> fetch()) {
-//        echo $registro['nome'] . '<br>';
-//    }
-//    
-//} catch(PDOException $e) {
-//    echo 'Erro ao conectar: ' . $e -> getMessage();
-//}
-
-
-// antigo
-//include_once "./conexao.php";
-//
-//try {
-//    $nome = 'f';
-//    $query = $conexao -> prepare("SELECT * from pessoa "
-//            . "WHERE tbl_pessoa_nome like ?");
-//    $query ->bindValue(1, $nome . '%');
-//    $query -> execute();
-//    while($registro = $query -> fetch()) {
-//        echo $registro['tbl_pessoa_nome'] . '<br>';
-//    }
-//    
-//} catch(PDOException $e) {
-//    echo 'Erro ao conectar: ' . $e -> getMessage();
-//}
-?>
